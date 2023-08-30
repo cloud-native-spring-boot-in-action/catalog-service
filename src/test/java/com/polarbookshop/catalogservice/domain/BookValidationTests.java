23fa0c3d04f4a192223331c3318815ec8995666c
Nilvandro Muianga <nbcmuianga@gmail.com>
2023-08-30 22:13:05 +0200

    chore: Create Unit Tests

---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -0,0 +1,38 @@
+package com.polarbookshop.catalogservice.domain;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+class BookValidationTests {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds() {
+        var book = new Book("1234567892", "Atomic Habits", "James Clear", 20.0);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        Assertions.assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenIsbnIsDefinedButIncorrectThenValidationFails() {
+        var book = new Book("12345", "Atomic Habits", "James Clear", 20.0);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        Assertions.assertThat(violations).hasSize(1);
+        Assertions.assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("The ISBN format must be valid.");
+    }
+}
